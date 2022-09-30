e87ce2b4c1281370de6d607053b592f9be98c4f4
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-09-30 11:17:35 -0500

    Added Navbar component file

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {AppBar, Box} from "@mui/material";
+
+const Navbar = () => {
+    const sections = ['Home', 'About me', 'Contact me'];
+
+    return (
+        <Box>
+            <AppBar>
+
+            </AppBar>
+        </Box>
+    );
+}
+
+export default Navbar;
